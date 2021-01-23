a5c9e1def121e5be065b32974cde21f8e7c4de2b
Eduardo ES Riccardi <ed@kukino.uk>
2021-01-23 00:27:00 +0000

    update to perftest

--- stream_mmap_test.go
+++ stream_mmap_test.go
@@ -22,23 +22,29 @@ func testSimpleCreateOpenFeedDelete(t *testing.T) {
 		t.Fatal(err)
 	}
 
+	//gob.Register(map[string]string{})
+	//value := map[string]string{"a": "b"}
+	value := []byte("hello how are you doing?")
+
 	count := 10 * 1024 * 1024
 	for i := 0; i < count; i++ {
 		if i%1000000 == 0 {
 			fmt.Println(i)
 		}
-		s.Feed(i)
+		s.Feed(value)
 	}
 
 	dt := time.Now().Sub(t0)
-	fmt.Printf("Took: %v to store %v M.records, avg. %v/record, %v Mb.\n",
-		dt, count/1024/1024, time.Duration(int64(dt)/int64(count)), s.descriptor.Write/1024/1024)
+	rt := time.Duration(int64(dt) / int64(count))
+	rps := 1_000_000_000 / rt.Nanoseconds()
+	fmt.Printf("Took: %v to store %v MElems, avg. %v/write, %v IOPS, %v Mb.\n",
+		dt.Truncate(time.Millisecond), count/1024/1024, rt, rps, s.descriptor.Write/1024/1024)
 
 	if err = s.CloseFile(); err != nil {
 		t.Fatal()
 	}
 
-	// Took: 27.964029808s to store 10 M.records, avg. 2.666µs/record, 221 Mb.
+	// Took: 29.527s to store 10 MElems, avg. 2.815µs/write, 355239 IOPS, 470 Mb.
 }
 
 func cleanup(prefix string) {
