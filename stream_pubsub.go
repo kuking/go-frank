a419357654142362f013730f7da27b6bb6911bfb
Eduardo ES Riccardi <ed@kukino.uk>
2021-01-27 19:31:33 +0000

    update to notes

--- stream_pubsub.go
+++ stream_pubsub.go
@@ -6,25 +6,18 @@ package go_frank
 // Reads, writes text files, each element is one line; end of line characters are stripped when reading, added when
 // writing. Options: append=true (only used in publishers). Useful for reading CSV files, one line JSONs.
 //
-// file+mm://
-// Memory mapped file; multiple producer, multiple consumer stream. Parameters:
-// - offset: resets reading offset to the provided element offset
-// - subscriberId: uniq id used to remember subscriber offset position
-// - extendSize: how big are going to be allowed to be each extend (i.e. 10M) only used when creating a new mm file
+// file+frank://
+// A Frank memory mapped file; multiple producer, multiple consumer stream. Parameters:
+// - subscriberId=string: uniq id used to continue, track offset position in the stream
+// - reset=boolean: resets the stream position to its earliest possible
+// - partSize=megabytes: how big are going to be each part file, only configurable at creation time
 //
-// A memory mapped files is actually multiple files: i.e. file+mm://events
-// - events.001, events.001, events.002 are the file extends
-// - events.idx offset indexes for fast skipping
-// - events.cli clients states
-//
-// concurrency warranties are valid as far as the underlying operating system honours the memory...
-// https://github.com/edsrzf/mmap-go
-// https://github.com/riobard/go-mmap
+// concurrency warranties are valid as far as the underlying operating system honours sharing memory buffers.
 //
 // mem://name
 // In memory stream, behaves similarly as EmptyStream(n int). Useful for parametrised initialisation. If a name is
 // provided, it will be registered and shared between different instances. Parameters:
-// - size: number of elements to hold
+// - size=n: number of elements to hold
 // - multicast: boolean, enables multicasting in pub-sub.
 //
 // udp+recv://bind_host:listen_port
