6977abc637228c0eaa76143281b14c813ab39fed
Eduardo ES Riccardi <ed@kukino.uk>
2021-02-15 19:26:00 +0000

    buffered writer

--- transport/buffered_conn.go
+++ transport/buffered_conn.go
@@ -7,15 +7,16 @@ import (
 
 type BufferedConn struct {
 	r *bufio.Reader
+	w *bufio.Writer
 	net.Conn
 }
 
 func NewBufferedConn(c net.Conn) BufferedConn {
-	return BufferedConn{bufio.NewReader(c), c}
+	return BufferedConn{bufio.NewReader(c), bufio.NewWriter(c), c}
 }
 
 func NewBufferedConnSize(c net.Conn, n int) BufferedConn {
-	return BufferedConn{bufio.NewReaderSize(c, n), c}
+	return BufferedConn{bufio.NewReaderSize(c, n), bufio.NewWriterSize(c, n), c}
 }
 
 func (b BufferedConn) Peek(n int) ([]byte, error) {
@@ -24,4 +25,23 @@ func (b BufferedConn) Peek(n int) ([]byte, error) {
 
 func (b BufferedConn) Read(p []byte) (int, error) {
 	return b.r.Read(p)
+}
+
+func (b BufferedConn) Write(p []byte) (int, error) {
+	return b.w.Write(p)
+}
+
+func (b *BufferedConn) WriteByte(c byte) error {
+	return b.w.WriteByte(c)
+}
+
+func (b *BufferedConn) Flush() error {
+	return b.w.Flush()
+}
+func (b *BufferedConn) WriteRune(r rune) (size int, err error) {
+	return b.w.WriteRune(r)
+}
+
+func (b *BufferedConn) WriteString(s string) (int, error) {
+	return b.w.WriteString(s)
 }
